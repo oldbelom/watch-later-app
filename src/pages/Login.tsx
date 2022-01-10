092c7a363043296b182f2c13d17bab2bad3f5890
oldbelom <oldbelom@gmail.com>
2022-01-10 22:21:45 +0300

    update Login and Signin components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,48 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import firebase from "firebase";
 import Button from "../components/Button/Button";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
 
-  const login = async () => {
+  const login = async (e: FormEvent) => {
+    e.preventDefault();
     await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        if (errorCode === "auth/weak-password") {
-          alert("The password is too weak.");
-        } else {
-          alert(errorMessage);
+        if (error.code === "auth/weak-password") {
+          setErrorText("Пароль должен быть не менее 6 символов");
+        } else if (error.code === "auth/invalid-email") {
+          setErrorText("Неверный фомат электронной почты");
+        } else if (error.code === "auth/email-already-in-use") {
+          setErrorText("Пользователь с таким e-mail уже существует");
         }
       });
   };
 
   return (
-    <div>
-      <h1>РЕГИСТРАЦИЯ</h1>
-      <label>
-        Введите email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Введите пароль
-        <input
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <Button onClick={login} text="Зарегистрироваться" />
-    </div>
+    <form className="container auth-form" onSubmit={login}>
+      <h3 className="auth-form__title">Регистрация</h3>
+      <input
+        type="text"
+        placeholder="Введите e-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Введите пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div className="auth-form__btn-block">
+        <Button type="submit" text="Зарегистрироваться" />
+        {errorText ? <p className="auth-form__error">{errorText}</p> : null}
+      </div>
+    </form>
   );
 };
 
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,52 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import firebase from "firebase";
 import Button from "../components/Button/Button";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
 
-  const signin = async () => {
+  const signin = async (e: FormEvent) => {
+    e.preventDefault();
     await firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        if (errorCode === "auth/wrong-password") {
-          alert("Wrong password.");
-        } else {
-          alert(errorMessage);
+        if (error.code === "auth/wrong-password") {
+          setErrorText("Неверный пароль");
+        } else if (error.code === "auth/invalid-email") {
+          setErrorText("Неверный фомат электронной почты");
+        } else if (error.code === "auth/user-not-found") {
+          setErrorText("Пользователь не найден");
         }
-        console.log(error);
       });
   };
 
   return (
-    <div>
-      <h1>ВХОД !</h1>
-      <Button onClick={signin} text="Войти" />
-      <div>
-        <label>
-          Введите email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
+    <form className="container auth-form" onSubmit={signin}>
+      <h3 className="auth-form__title">Вход</h3>
+      <input
+        type="text"
+        placeholder="Введите e-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Введите пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div className="auth-form__btn-block">
+        <Button type="submit" text="Войти" />
+        {errorText ? <p className="auth-form__error">{errorText}</p> : null}
       </div>
-      <div>
-        <label>
-          Введите пароль
-          <input
-            type="text"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-      </div>
-    </div>
+    </form>
   );
 };
 
